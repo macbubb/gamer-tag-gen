1d530d52740a9faed82f42af4c8854f2982e04cb
Ernest Bubb <bubbem@gmail.com>
2021-02-26 14:06:24 -0800

    removed unncessary states and created single state array of gamer tag objects

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 import { adjs, nouns } from './Constants';
 
 function App() {
-  const [tag, setTag] = useState("");
-  const [tagInventory, setTagInventory] = useState([]); //add objects to array
-  const tempTag = {
-    timeStamp:'',
-    tag:'',
-    stars:'',
-    color: ''
-  };  //doesn't need to be part of state just a place to store latest
+  const [tagInventory, setTagInventory] = useState([]);
 
-  //need to make an array of objects for gamer tags including key as timestamp, stars, color, and stars
+  const handleAdd = (tag) => {
+    setTagInventory([...tagInventory, tag]);
+  }
 
-  const makeTag = (adjs, nouns) => {
+  // function for picking color
+
+  const makeTag = () => {
     const randomAdjIndex = Math.floor(Math.random()*adjs.length);
     const randomNounIndex = Math.floor(Math.random()*nouns.length);
-    const stars = 0;
-    const color = '#ffffff';
     var timeStamp = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
-    const tag = adjs[randomAdjIndex] + nouns[randomNounIndex];
-    return { timeStamp, tag, stars, color };
+    const newTag = {
+      timeStamp: timeStamp,
+      tag : adjs[randomAdjIndex] + nouns[randomNounIndex],
+      stars : 0,
+      color : '#ffffff',
+    };
+    handleAdd(newTag);
+    console.log(newTag);
   }
 
-  const handleClick = () => {
-    tempTag = {...makeTag(adjs, nouns)};
-    //setTag(newTag);
-    /*setNewTag({
-      ...longTag,
-      tag: newTag
-    });*/
-    //setTagInventory([...tagInventory, newTag]);
-    console.log(tempTag);
-  }
-
-  //setNewTag({...newTag, tag: 'stinkyBottom'});
-
-// need method to render tag inventory
+ // need method to render tag inventory
 
   return (
     <main>
       <h1>Gamer Tag Brain Storm Factory</h1>
       <div class="flexIt">
       <div class="left">
-        <h2>{tag}</h2>
-        <button onClick={() => handleClick()}>New Tag</button>
+        <h2></h2>
+        <button onClick={() => makeTag()}>New Tag</button>
       </div>
       <div class="right">
         <ol>
-          {tagInventory.map( (item) => <li>{item} </li>) }
+          {tagInventory.map( (item) => <li key={item.timeStamp}>{item.tag} </li>) }
         </ol>
       </div>
       </div>
