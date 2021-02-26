3426b2538eef8fbe0c132edcca41772420aa480d
Ernest Bubb <bubbem@gmail.com>
2021-02-26 12:19:20 -0800

    making gamer tag and methods objects

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import { adjs, nouns } from './Constants';
 
 function App() {
   const [tag, setTag] = useState("");
-  const [tagInventory, setTagInventory] = useState([]); //turn into array
-  const [longTag, setNewTag] = useState({
+  const [tagInventory, setTagInventory] = useState([]); //add objects to array
+  const tempTag = {
     timeStamp:'',
-    tag:'stinkyBottom',
-    stars: 0,
-    color: '#ffffff'
-  });  //doesn't need to be part of state just a place to store latest
+    tag:'',
+    stars:'',
+    color: ''
+  };  //doesn't need to be part of state just a place to store latest
 
   //need to make an array of objects for gamer tags including key as timestamp, stars, color, and stars
 
   const makeTag = (adjs, nouns) => {
     const randomAdjIndex = Math.floor(Math.random()*adjs.length);
     const randomNounIndex = Math.floor(Math.random()*nouns.length);
-    // add stars
-    // add colors
-    // add timestamp
-    return adjs[randomAdjIndex] + nouns[randomNounIndex];
+    const stars = 0;
+    const color = '#ffffff';
+    var timeStamp = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
+    const tag = adjs[randomAdjIndex] + nouns[randomNounIndex];
+    return { timeStamp, tag, stars, color };
   }
 
   const handleClick = () => {
-    const newTag = makeTag(adjs, nouns);
-    setTag(newTag);
-    setNewTag({
+    tempTag = {...makeTag(adjs, nouns)};
+    //setTag(newTag);
+    /*setNewTag({
       ...longTag,
       tag: newTag
-    });
-    setTagInventory([...tagInventory, newTag]);
-    console.log(tagInventory);
+    });*/
+    //setTagInventory([...tagInventory, newTag]);
+    console.log(tempTag);
   }
 
   //setNewTag({...newTag, tag: 'stinkyBottom'});
