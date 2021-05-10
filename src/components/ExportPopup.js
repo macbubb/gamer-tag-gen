dfbce735dc9b6f9922868bc0f17745c5f0541f1c
Ernest Bubb <bubbem@gmail.com>
2021-05-10 09:36:58 -0700

    updated export and popup, triggers with button, svg still weird

--- src/components/ExportPopup.js
+++ src/components/ExportPopup.js
@@ -4,11 +4,18 @@ import html2canvas from 'html2canvas';
 
 const ExportPopup = (props) => {
   const { card, changeStars, deleteCard, cardDimensions, palette } = props;
-  useEffect(() => takeScreenShot(), [Card]);
+  //  useEffect(() => takeScreenShot(), []);
   var newWindow = window.open('', 'exportPopup');
   const takeScreenShot = () => {
     let id = `#${card.tag}`;
     console.log(id);
+    var svgElements = newWindow.document.body.querySelectorAll('svg');
+    svgElements.forEach(function (item) {
+      item.setAttribute('width', item.getBoundingClientRect().width);
+      item.setAttribute('height', item.getBoundingClientRect().height);
+      item.style.width = null;
+      item.style.height = null;
+    });
     html2canvas(newWindow.document.querySelector(id), {}).then((canvas) => {
       canvas.toBlob((blob) => {
         var file = new File([blob], `${card.tag}.png`, {
@@ -31,6 +38,15 @@ const ExportPopup = (props) => {
         newTag={true}
         deleteCard={deleteCard}
       />
+      <div className="save-button">
+        <button
+          data-html2canvas-ignore
+          onClick={() => takeScreenShot()}
+          className="save"
+        >
+          SAVE
+        </button>
+      </div>
     </div>
   );
 };
